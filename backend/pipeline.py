49365167179b9252a4cf5bb6622d56e9c5cf9e25
anaprates-r <ana.prates@ufms.br>
2026-04-08 14:40:32 -0400

    teste

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -25,21 +25,28 @@ def etl(fileName):
     print(f"DEBUG: Chaves do dicionário: {registros[0].keys()}")
 
     with app.app_context():
+        try:
         # 2. Prepara a instrução de inserção
-        stmt = insert(Medicamento).values(registros)
+            stmt = insert(Medicamento).values(registros)
 
         # 3. Configura o Upsert (ON CONFLICT DO UPDATE)
-        upsert_stmt = stmt.on_conflict_do_update(
-            # Nome da constraint ou colunas que formam a chave única
-            index_elements=['catmat', 'estabelecimento_saude'],
+            upsert_stmt = stmt.on_conflict_do_update(
+                # Nome da constraint ou colunas que formam a chave única
+                index_elements=['catmat', 'estabelecimento_saude'],
             
             # O que atualizar se o registro já existir
-            set_={
-                'quantidade': stmt.excluded.quantidade,
-                'medicamento': stmt.excluded.medicamento
-            }
-        )
+                set_={
+                    'quantidade': stmt.excluded.quantidade,
+                    'medicamento': stmt.excluded.medicamento
+                }
+            )
 
         # 4. Executa no banco em uma única transação
-        db.session.execute(upsert_stmt)
-        db.session.commit()
+            db.session.execute(upsert_stmt)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print("--- ERRO NO BANCO DE DADOS ---")
+            print(str(e)) # Isso vai dizer se falta coluna ou se o índice não existe
+            print("-------------------------------")
+        
